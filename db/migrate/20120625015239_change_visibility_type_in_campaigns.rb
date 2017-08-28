309aa445d4f2a54f7d6715430083d8b50739650e
Matt Cramp <mattcrampy@gmail.com>
2017-08-29 01:23:23 +1000

    Updated campaign visibility to use booleans

--- db/migrate/20120625015239_change_visibility_type_in_campaigns.rb
+++ db/migrate/20120625015239_change_visibility_type_in_campaigns.rb
@@ -1,9 +1,18 @@
 class ChangeVisibilityTypeInCampaigns < ActiveRecord::Migration[4.2]
+ 	class Campaign < ActiveRecord::Base
+ 		#ensures this migration will run even if campaign class is renamed
+ 	end
+
   def up
-  	change_column :campaigns, :visibility, :integer
+  	add_column :campaigns, :is_public, :boolean, default: true
+  	Campaign.where(visibility: "closed").update_all(is_public: false)
+  	remove_column :campaigns, :visibility
   end
 
   def down
-  	change_column :campaigns, :visibility, :string
+  	add_column :campaigns, :visibility, :string
+  	Campaign.where(is_public: true).update_all(visibility: "open")
+  	Campaign.where(is_public: false).update_all(visibility: "closed")
+  	remove_column :campaigns, :is_public
   end
 end
